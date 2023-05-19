14c31907e3bd3aa42d44a1362d0a99cd2b9f88b7
Daniel Fragoso <ing.fragoso94@gmail.com>
2023-05-18 21:53:02 -0600

    se implemento la compra con room en DetailFragment

--- app/src/main/java/com/my/first/elearningapp/fragments/DetailFragment.kt
+++ app/src/main/java/com/my/first/elearningapp/fragments/DetailFragment.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.coroutineScope
+import androidx.room.Room
 import com.my.first.elearningapp.R
+import com.my.first.elearningapp.database.ElearningDatabase
+import com.my.first.elearningapp.database.entities.ShoppingEntity
+import com.my.first.elearningapp.database.entities.UserEntity
 import com.my.first.elearningapp.databinding.FragmentDetailBinding
 import com.my.first.elearningapp.model.COURSE_ID
 import com.my.first.elearningapp.model.Course
 import com.my.first.elearningapp.model.CourseClickListener
 import com.my.first.elearningapp.model.listCourses
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DetailFragment : Fragment(R.layout.fragment_detail), CourseClickListener {
 
     private lateinit var binding: FragmentDetailBinding
-    private var myIdCourses = mutableListOf<Int>()
+    //private var myIdCourses = mutableListOf<Int>()
+
+    private lateinit var database: ElearningDatabase
 
     override fun onClick(course: Course)
     {
@@ -26,7 +35,8 @@ class DetailFragment : Fragment(R.layout.fragment_detail), CourseClickListener {
 
         binding = FragmentDetailBinding.bind(view)
 
-        var bought = false
+        initDatabase(view)
+        //var bought = false
 
         val courseID = arguments?.getInt(COURSE_ID, -1)!!
         val course = courseFromID(courseID)
@@ -41,8 +51,25 @@ class DetailFragment : Fragment(R.layout.fragment_detail), CourseClickListener {
         }
 
         binding.buttonBuy.setOnClickListener{
-
-            if(course != null) {
+            lifecycle.coroutineScope.launch(Dispatchers.IO) {
+                val user = getUserLogin()
+                if (user != null){
+                    val courseIds = getCourseShopping(user.id)
+                    lifecycle.coroutineScope.launch(Dispatchers.Main) {
+                        if(course != null){
+                            val bought = courseIds.contains(course.id) //Verificamos si el curso se encuentra en nuestra lista de cursos comprados.
+                            if (bought){
+                                buyCourse(course.id, user.id)
+                                Toast.makeText(context,"Haz adquirido el curso de ${course.name}", Toast.LENGTH_SHORT).show()
+                            }
+                            else{
+                                Toast.makeText(context,"Ya has adquirido este curso", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }
+            }
+            /*if(course != null) {
                 for(id in myIdCourses) {
                     if(id == course.id)
                         bought = true
@@ -56,8 +83,34 @@ class DetailFragment : Fragment(R.layout.fragment_detail), CourseClickListener {
                     Toast.makeText(context,"Ya has adquirido este curso", Toast.LENGTH_SHORT).show()
                 }
 
-            }
+            }*/
         }
+    }
+
+
+    private fun initDatabase(view: View){
+        database = Room.databaseBuilder(
+            view.context, ElearningDatabase::class.java, ElearningDatabase.DATABASE_NAME)
+            .allowMainThreadQueries()
+            .build()
+    }
+
+    private suspend fun buyCourse(courseID: Int, userID: Int){
+        val request = ShoppingEntity(
+            idUser = userID,
+            idCourse = courseID
+        )
+        database.getUserDao().insertShopping(request)
+    }
+
+    private suspend fun getCourseShopping(idUser: Int): List<Int>{
+        var response = database.getUserDao().getAllShopping(idUser)
+        return response
+    }
+
+    private suspend fun getUserLogin(): UserEntity? {
+        var response = database.getUserDao().getUserLogin()
+        return response
     }
 
     private fun courseFromID(courseID: Int): Course? {
