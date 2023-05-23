af727025c2d39c2cdb23d8d454f177338b0af464
Daniel Fragoso <ing.fragoso94@gmail.com>
2023-05-22 20:12:43 -0600

    se modificaron el diseño del fragment_mycourse y se corrigio detalle en la activity mycourse

--- app/src/main/java/com/my/first/elearningapp/update/UpdateActivity.kt
+++ app/src/main/java/com/my/first/elearningapp/update/UpdateActivity.kt
@@ -1,9 +1,11 @@
 package com.my.first.elearningapp.update
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.coroutineScope
 import androidx.room.Room
 import com.google.android.material.button.MaterialButton
 import com.my.first.elearningapp.MainActivity
@@ -25,55 +27,72 @@ class UpdateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-        btnUpdate =findViewById(R.id.btn_update2)
-        etName = findViewById(R.id.et_name_profile)
-        etEmail = findViewById(R.id.et_email_address_profile)
-        etMobile = findViewById(R.id.et_mobile_profile)
 
+        initUI()
+        lifecycle.coroutineScope.launch(Dispatchers.IO) {
+            initData2()
+            lifecycle.coroutineScope.launch(Dispatchers.Main){
+                btnUpdate.setOnClickListener {
+                    val SinglePass = UserData.userPass.toString().trim()
+                    val name = etName.text.toString().trim()
+                    val email = etEmail.text.toString().trim()
+                    val mobile = etMobile.text.toString().trim()
+
+                    if (name.isNotEmpty() && email.isNotEmpty() && mobile.isNotEmpty()) {
+                        lifecycle.coroutineScope.launch(Dispatchers.Main) {
+                            val user1 = database.getUserDao().getUserId(UserData.userEmail)
+                            user1.name = name
+                            user1.email = email
+                            user1.mobile = mobile
+                            database.getUserDao().update(user1)
+                            Toast.makeText(applicationContext, "Data updated successfully", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(applicationContext, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+
+        }
+
+    private fun initUI(){
         database = Room.databaseBuilder(
             applicationContext, ElearningDatabase::class.java, ElearningDatabase.DATABASE_NAME
         ).allowMainThreadQueries()
             .build()
 
-        GlobalScope.launch(Dispatchers.Main) {
-            initData2()
-
-        }
-
-       btnUpdate.setOnClickListener {
-            val SinglePass = UserData.userPass.toString().trim()
-            val name = etName.text.toString().trim()
-            val email = etEmail.text.toString().trim()
-            val mobile = etMobile.text.toString().trim()
-
-
-            if (name.isNotEmpty() && email.isNotEmpty() && mobile.isNotEmpty()) {
-                GlobalScope.launch(Dispatchers.Main) {
-                    val user1 = database.getUserDao().getUserId(UserData.userEmail)
-                    user1.name = name
-                    user1.email = email
-                    user1.mobile = mobile
-                    database.getUserDao().update(user1)
-                    Toast.makeText(applicationContext, "Data updated successfully", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(applicationContext, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
-            }
-
-            }
-
-
-        }
+        btnUpdate =findViewById(R.id.btn_update2)
+        etName = findViewById(R.id.et_name_profile)
+        etEmail = findViewById(R.id.et_email_address_profile)
+        etMobile = findViewById(R.id.et_mobile_profile)
+    }
 
 
     private suspend fun initData2() {
-        val SingleUser = UserData.userEmail.toString()
-        val user = database.getUserDao().getUserId(SingleUser.toString())
-        etName.hint = user.name
-        etEmail.hint = user.email
-        etMobile.hint = user.mobile
+        try{
+            val SingleUser = UserData.userEmail.toString()
+            val user = database.getUserDao().getUserId(SingleUser.toString())
+            etName.setText(user.name)
+            etEmail.setText(user.email)
+            etMobile.setText(user.mobile)
+        }
+        catch (e: Exception)
+        {
+            runOnUiThread {
+                Toast.makeText(applicationContext, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+
+        // Asegúrate de finalizar la actividad actual para que no se pueda volver atrás
+        finish()
     }
+}
 
 
 
