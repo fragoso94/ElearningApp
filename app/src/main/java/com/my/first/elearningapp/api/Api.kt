f3e714c1070ce00001deae5ed94f80505a6a0678
Daniel Fragoso <ing.fragoso94@gmail.com>
2023-06-23 17:49:56 -0600

    Se modifico el método para consumir la api en producción y se agrego un interceptor para omitir el https no seguro

--- app/src/main/java/com/my/first/elearningapp/api/Api.kt
+++ app/src/main/java/com/my/first/elearningapp/api/Api.kt
@@ -1,6 +1,7 @@
 package com.my.first.elearningapp.api
 
 import com.my.first.elearningapp.database.utilities.Helpers
+import com.my.first.elearningapp.database.utilities.Helpers.Companion.createUnsafeOkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,6 +10,7 @@ object Api {
     val courseService: ApiService =
         Retrofit.Builder()
             .baseUrl(Helpers.URL_BASE_API + "api/")
+            .client(createUnsafeOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
--- app/src/main/java/com/my/first/elearningapp/database/utilities/Helpers.kt
+++ app/src/main/java/com/my/first/elearningapp/database/utilities/Helpers.kt
@@ -2,17 +2,22 @@ package com.my.first.elearningapp.database.utilities
 
 import android.content.Context
 import android.content.res.Resources
-import android.util.Log
-import com.my.first.elearningapp.R
 import com.my.first.elearningapp.model.Course
 import com.my.first.elearningapp.model.CourseResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.security.KeyManagementException
+import java.security.NoSuchAlgorithmException
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class Helpers {
     companion object{
         const val COURSE_ID = "courseID"
         const val COURSE_ITEM = "courseITEM"
         const val IS_VIEW_BUY = "courseTYPE"
-        const val URL_BASE_API = "http://192.168.1.164:9095/"
+        const val URL_BASE_API = "https://apicursosbedu.servicesnet.site/" //"http://192.168.1.164:9095/"
 
         fun convertDataClass(context: Context, dataClassA: CourseResponse): Course {
             val resources: Resources = context.resources
@@ -39,6 +44,39 @@ class Helpers {
             }
 
             return courses
+        }
+
+        fun createUnsafeOkHttpClient(): OkHttpClient {
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<out java.security.cert.X509Certificate>?, authType: String?) {
+                }
+
+                override fun checkServerTrusted(chain: Array<out java.security.cert.X509Certificate>?, authType: String?) {
+                }
+
+                override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
+                    return arrayOf()
+                }
+            })
+
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            return try {
+                val sslContext = SSLContext.getInstance("TLS")
+                sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+                val sslSocketFactory = sslContext.socketFactory
+
+                OkHttpClient.Builder()
+                    .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                    .addInterceptor(loggingInterceptor)
+                    .hostnameVerifier { _, _ -> true }
+                    .build()
+            } catch (e: NoSuchAlgorithmException) {
+                throw RuntimeException("Failed to create unsafe OkHttpClient", e)
+            } catch (e: KeyManagementException) {
+                throw RuntimeException("Failed to create unsafe OkHttpClient", e)
+            }
         }
 
     }
